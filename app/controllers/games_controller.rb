1620d9b4c738a9fc6b3965fa4b87fdb2f314c1c8
Ines Alvergne <i.alvergne@gmail.com>
2022-11-06 12:56:13 -0500

    logic of the game works

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,40 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
+
   def new
-    letters = ("A".."Z").to_a
-    @letters_array = 9.times.map { letters.sample }
+    @letters = 9.times.map { ('A'..'Z').to_a.sample }
+  end
+
+ # The word is valid according to the grid, but is not a valid English word
+  def exist?(word)
+    # API endpoint: https://wagon-dictionary.herokuapp.com/table
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    string_serialized = URI.open(url).read
+    result = JSON.parse(string_serialized)
+    result['found']
+  end
+
+  # The word can’t be built out of the original grid
+  def valid?(word, letters)
+    word_array = word.chars
+    word_array.all? { |w| letters.include?(w) }
   end
 
   def score
-    @score = params[:word]
+    # The word can’t be built out of the original grid
+    # The word is valid according to the grid, but is not a valid English word
+    # The word is valid according to the grid and is an English word
+    @input = params[:word].upcase
+    @letters = params[:letters].split
+    @result = ''
+    if valid?(@input, @letters) && exist?(@input)
+      @result = "Congratulations! #{@input} is a valid English word!"
+    elsif valid?(@input, @letters) && exist?(@input) == false
+      @result = "Sorry but #{@input} is not a valid English word"
+    elsif exist?(@input) && valid?(@input, @letters) == false
+      @result = "Sorry but #{@input} can't be build out of #{@letters_array}"
+    end
   end
 end
